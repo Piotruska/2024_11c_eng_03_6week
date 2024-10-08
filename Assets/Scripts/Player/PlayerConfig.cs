f7cdce208b09142791358bd78536673123559768
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-10-08 18:21:07 +0200

    Added Player Config
    Added player config file with speed jump force and extra jump variables

---
+++ Assets/Scripts/Player/PlayerConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Player Config",fileName = "Player config")]
+public class PlayerConfig : ScriptableObject
+{
+    public float movementSpeed = 2.0f;
+    public float jumpForce = 2.0f;
+    public int extraJumps = 1;
+}

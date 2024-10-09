43a491b713e88606d339df83715435bd7fd915d1
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-10-09 15:48:12 +0200

    Added methods for Respawn Mechanic
    Die(), Respawn() and added default implementation

--- Assets/Scripts/Player/GameController.cs
+++ Assets/Scripts/Player/GameController.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 _startPosition;
+    private SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField]private float _respawnDuration = 0.5f;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
+        _startPosition = transform.position;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("InstantDeath"))
+        {
+            Die();
+        }
     }
+
+    private void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    private IEnumerator Respawn(float duration)
+    {
+        _spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
+        transform.position = _startPosition;
+        _rigidbody2D.velocity = new Vector2(0, 0);
+        _spriteRenderer.enabled = true;
+    }
+    
 }

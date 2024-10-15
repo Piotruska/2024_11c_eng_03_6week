52cce15fe7e3616b2387d8050604445bed5b4568
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-10-15 19:12:21 +0200

    Joined Player Jump And movement Mechanic Script
    Fixed some syntax errors and made the script more reliable

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,21 +6,41 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private float _xInput;
+    private bool _perform_jump ;
+    private bool _isGrounded;
+    private int extraJumpsValue;
+    private KeyCode _jumpKeyCode = KeyCode.UpArrow;
     [SerializeField] private PlayerConfig _config;
-    void Start()
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _checkRadious;
+    [SerializeField] private LayerMask _whatIsGround; 
+    void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        extraJumpsValue = _config.extraJumpCount;
     }
 
     void Update()
     {
         _xInput = Input.GetAxis("Horizontal");
         
+        if (_isGrounded)
+        {
+            extraJumpsValue = _config.extraJumpCount;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumpsValue >0)
+        {
+            _rb.velocity = Vector2.up* _config.jumpForce;
+            extraJumpsValue--;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumpsValue == 0 && _isGrounded){
+            _rb.velocity = Vector2.up * _config.jumpForce;
+        }
+        
     }
-    
     void FixedUpdate()
     {
-        // Fix config
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _checkRadious, _whatIsGround);
         _rb.velocity = new Vector2(_xInput * _config.movementSpeed, _rb.velocity.y);
     }
 }

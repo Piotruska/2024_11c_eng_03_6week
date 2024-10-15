fab216090c0c6004cb7abe054eec488c12474e11
Red <mark.matveyenka@proton.me>
2024-10-15 13:14:59 +0200

    Update Coin collect method

--- Assets/Scripts/Collectibles/Coin.cs
+++ Assets/Scripts/Collectibles/Coin.cs
@@ -1,10 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
 namespace Collectibles
 {
     public class Coin : Collectible
     {
+        [SerializeField] private CoinConfig _coinConfig;
         protected override void Collect()
         {
-            
+            Destroy(gameObject);
         }
     }
 }

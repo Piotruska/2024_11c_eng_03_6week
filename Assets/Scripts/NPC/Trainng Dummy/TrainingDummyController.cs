65da929f98e8539b2548a6131d1e488c93a2946c
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-10-17 19:16:20 +0200

    Moved Training dummy to NPC files,

--- Assets/Scripts/NPC/Trainng Dummy/TrainingDummyController.cs
+++ Assets/Scripts/NPC/Trainng Dummy/TrainingDummyController.cs
@@ -7,7 +7,7 @@ namespace Trainng_Dummy
     public class TrainingDummyController  : MonoBehaviour, IDamageable
     {
         [Header("Health")] 
-        [SerializeField] private float _maxHealth = 2f;
+        [SerializeField] private float _maxHealth = 20.0f;
         private float _currentHealth;
         private Animator _animator;
 
@@ -17,10 +17,31 @@ namespace Trainng_Dummy
             _currentHealth = _maxHealth;
         }
 
+        private void FixedUpdate()
+        {
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
         public void Hit(float damageAmount)
         {
             _animator.SetTrigger("Hit");
             _currentHealth -= damageAmount;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("InstantDeath"))
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
         }
     }
 }

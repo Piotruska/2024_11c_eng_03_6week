9e06a92500319b35909b204b36057e9408580068
Red <mark.matveyenka@proton.me>
2024-10-19 13:27:40 +0200

    tied chest animation to trigger action

--- Assets/Scripts/Interactables/Chest.cs
+++ Assets/Scripts/Interactables/Chest.cs
@@ -4,12 +4,18 @@ namespace Interactables
 {
     public class Chest : MonoBehaviour, IInteractable
     {
+        private Animator _animator;
         private bool _isUnlocked = false;
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
         
         public void OnInteractAction()
         {
+            _animator.SetTrigger("Unlock");
             _isUnlocked = true;
-            // Play animation and get some items
         }
     }
 }

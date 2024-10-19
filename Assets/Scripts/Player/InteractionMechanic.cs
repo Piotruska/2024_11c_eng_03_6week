71e62d6c07ed9340af877c657afe3cf34ba912a9
Red <mark.matveyenka@proton.me>
2024-10-19 12:54:24 +0200

    Implementing Interactable Scripts into Player

--- Assets/Scripts/Player/InteractionMechanic.cs
+++ Assets/Scripts/Player/InteractionMechanic.cs
@@ -5,6 +5,11 @@ namespace Player
 {
     public class InteractionMechanic : MonoBehaviour, ICanInteract
     {
+        [Header("Interaction Range Config")]
+        [SerializeField] private BoxCollider2D _interactionCollider;
+        [Header("Interactable Identification")] 
+        [SerializeField] public LayerMask _interactableLayer;
+        
         public void InteractStorage()
         {
         }

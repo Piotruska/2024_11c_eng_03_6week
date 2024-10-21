4be546f94635732258e7f8b396a709aca2975961
Red <mark.matveyenka@proton.me>
2024-10-21 19:48:47 +0200

    Added Key collect animation

--- Assets/Scripts/Collectibles/Key.cs
+++ Assets/Scripts/Collectibles/Key.cs
@@ -1,8 +1,23 @@
+using UnityEngine;
+
 namespace Collectibles
 {
     public class Key : ICollectible
     {
+        private Animator _animator;
+        private string _collectTrigger = "Collect";
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        
         protected override void Collect()
+        {
+            _animator.SetTrigger(_collectTrigger);
+        }
+        
+        private void Key_Despawn()
         {
             Destroy(gameObject);
         }

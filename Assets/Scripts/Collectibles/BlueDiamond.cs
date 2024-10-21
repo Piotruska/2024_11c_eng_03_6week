b9fea00c6b1ff4c993d7beb41a002e088143b379
Red <mark.matveyenka@proton.me>
2024-10-21 19:44:45 +0200

    Added Blue Diamond collect animation

--- Assets/Scripts/Collectibles/BlueDiamond.cs
+++ Assets/Scripts/Collectibles/BlueDiamond.cs
@@ -6,7 +6,20 @@ namespace Collectibles
     public class BlueDiamond : ICollectible
     {
         [SerializeField] private DiamondConfig _diamondConfig;
+        private Animator _animator;
+        private string _collectTrigger = "Collect";
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        
         protected override void Collect()
+        {
+            _animator.SetTrigger(_collectTrigger);
+        }
+        
+        private void BlueDiamond_Despawn()
         {
             Destroy(gameObject);
         }

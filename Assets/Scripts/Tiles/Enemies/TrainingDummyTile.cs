7f9c73227b634bc0fff77bb5aa03e5f08fa2db20
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-10-26 16:55:18 +0200

    Added Tiles for collectables

---
+++ Assets/Scripts/Tiles/Enemies/TrainingDummyTile.cs
@@ -0,0 +1,36 @@
+namespace Tiles.Collectibles
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Tilemaps;
+
+    [CreateAssetMenu(fileName = "TrainingDummyTile", menuName = "Tiles/TrainingDummy ")]
+    public class TrainingDummyTile : Tile
+    {
+        public GameObject TrainingDummyPrefab;
+
+        public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+        {
+            if (Application.isPlaying && TrainingDummyPrefab != null)
+            {
+                Tilemap tilemapComponent = tilemap.GetComponent<Tilemap>();
+                Vector3 worldPos = tilemapComponent.CellToWorld(position) + new Vector3(0.5f, 0.5f, 0);
+            
+                if (!PlatformExistsAtPosition(worldPos))
+                {
+                    GameObject platformInstance = Instantiate(TrainingDummyPrefab, worldPos, Quaternion.identity);
+                    platformInstance.transform.parent = tilemapComponent.transform;
+                }
+            
+                tilemapComponent.SetTile(position, null);
+            }
+            return base.StartUp(position, tilemap, go);
+        }
+
+        private bool PlatformExistsAtPosition(Vector3 position)
+        {
+            Collider2D collider = Physics2D.OverlapPoint(position);
+            return collider != null && collider.gameObject.CompareTag("TrainingDummy");
+        }
+    }
+}

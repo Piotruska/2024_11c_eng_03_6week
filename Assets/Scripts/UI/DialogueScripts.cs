81d1e57f7b0125412b75fca8a1b08442bbee54d0
Red <mark.matveyenka@proton.me>
2024-11-02 14:19:21 +0100

    Fixed DialogueScripts overwriting itself with each line

--- Assets/Scripts/UI/DialogueScripts.cs
+++ Assets/Scripts/UI/DialogueScripts.cs
@@ -20,21 +20,21 @@ namespace UI
         {
             List<string> newScript = new List<string>();
             // Script 0 - Test
-            newScript.Clear();
+            newScript = new List<string>();
             newScript.Add("This is a Test Script");
             Scripts.Add(newScript);
             // Script 1 - Tutorial 1
-            newScript.Clear();
+            newScript = new List<string>();
             newScript.Add("Ahoy, Matey! Haven't seen a livin' soul on dis island since forever. Anyway, the names Johnny!");
             newScript.Add("Well since I'm here already I might as well help ye out! Ye might 'ave figured it out already, but ye can use " + _movementKeys + " fer movement.");
             newScript.Add("Try it out 'n meet me a wee further away from here. Thar be much t' learn!");
             Scripts.Add(newScript);
             // Script 2 - Level End - Enough Diamonds
-            newScript.Clear();
+            newScript = new List<string>();
             newScript.Add("Enough Diamonds");
             Scripts.Add(newScript);
             // Script 3 - Level End - Not enough Diamonds
-            newScript.Clear();
+            newScript = new List<string>();
             newScript.Add("Not enough Diamonds");
             Scripts.Add(newScript);
         }

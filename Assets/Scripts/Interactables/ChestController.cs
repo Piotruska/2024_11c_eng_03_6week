685c3fbee1a6c78ba9469c6791dceb253678011a
Red <mark.matveyenka@proton.me>
2024-11-03 17:37:52 +0100

    Added an option to spawn an object on Chest unlock

--- Assets/Scripts/Interactables/ChestController.cs
+++ Assets/Scripts/Interactables/ChestController.cs
@@ -7,6 +7,11 @@ namespace Interactables
     {
         private Animator _animator;
         private bool _isUnlocked = false;
+        [Header("Item to spawn")]
+        [SerializeField]
+        private GameObject _item;
+        [SerializeField]
+        private Transform _itemSpawnPoint;
         
         private void Awake()
         {
@@ -15,12 +20,17 @@ namespace Interactables
         
         public void OnInteractAction()
         {
-            if (PlayerCollectibles.GetKeyCount() > 0)
+            if (PlayerCollectibles.GetKeyCount() > 0 && !_isUnlocked)
             {
                 PlayerCollectibles.DecreaseKeyCount(1);
                 _animator.SetTrigger("Unlock");
                 _isUnlocked = true;
             }
+        }
+
+        private void SpawnItem()
+        {
+            Instantiate(_item, _itemSpawnPoint.position, _itemSpawnPoint.rotation);
         }
     }
 }

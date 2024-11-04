3850d213dceaa1b1e30518b322e3b2ecf07d51a3
Red <mark.matveyenka@proton.me>
2024-11-04 21:31:49 +0100

    Added Tutorials 2 and 3

--- Assets/Scripts/UI/DialogueScripts.cs
+++ Assets/Scripts/UI/DialogueScripts.cs
@@ -8,7 +8,12 @@ namespace UI
         private static readonly List<List<string>> Scripts = new List<List<string>>();
         
         // Buttons
-        private string _movementKeys = "Arrow Keys"; 
+        private string _movementKeys = "Arrow Keys";
+        private string _jumpKey = "Z";
+        private string _attackKey = "X";
+        private string _dashKey = "C";
+        private string _item1Key = "A";
+        private string _item2Key = "S";
 
         public static List<string> GetScript(int index)
         {
@@ -40,6 +45,18 @@ namespace UI
             // Script 4 - Level End - Level already cleared
             newScript = new List<string>();
             newScript.Add("Go on then, mate. The next adventure awaits!");
+            Scripts.Add(newScript);
+            // Script 5 - Tutorial 2
+            newScript = new List<string>();
+            newScript.Add("Time fer yer next lesson!");
+            newScript.Add("See dis flag here? Dat's a checkpoint. Even if some evil fate befalls ye, ye'll always be able t' come back!");
+            newScript.Add("Now then, t' get over dis obstacle ye'll needs t' jump like a rabbit! Press the " + _jumpKey + " key 'n get to it. Ye can even jump twice, so use dis t' yer advantage!");
+            Scripts.Add(newScript);
+            // Script 6 - Tutorial 3
+            newScript = new List<string>();
+            newScript.Add("See dis bottle? Dat's a health potion. Ye can press " + _item1Key + " to use it 'n trust me ye'll needs it soon!");
+            newScript.Add("If ye continue fore ye'll encounter a quite devious creature, but do nah be afeared! Use everythin' ye know and ye should be able t' escape safely!");
+            newScript.Add("Oh, nigh-on forgot! Ye can dash by pressin' C, I be sure dat shall come handy in the comin' encounter.");
             Scripts.Add(newScript);
         }
     }

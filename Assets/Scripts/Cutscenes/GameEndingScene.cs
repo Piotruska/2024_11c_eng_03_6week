a2e3d3fdfd35f983abf93437115123be8ee016c0
Red <mark.matveyenka@proton.me>
2024-11-07 14:00:20 +0100

    Added BlackScreen canvas with FadeIn and FadeOut animations, Initial work for EndingScene

--- Assets/Scripts/Cutscenes/GameEndingScene.cs
+++ Assets/Scripts/Cutscenes/GameEndingScene.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UI;
 using UnityEngine;
 
@@ -7,13 +9,23 @@ namespace Cutscenes
     public class GameEndingScene : MonoBehaviour
     {
         private DialogueMenuDisplay _dialogueMenu;
-        // Start is called before the first frame update
+        private BlackScreen _blackScreen;
+        private GameObject _HUD;
+        
+        private GameObject _player;
+        
+        [SerializeField]
+        private Transform _playerTransform;
+        [SerializeField]
+        private Transform _johnnyTransform;
         private void Awake()
         {
             _dialogueMenu = FindObjectOfType<DialogueMenuDisplay>();
+            _blackScreen = FindObjectOfType<BlackScreen>();
+            _HUD = GameObject.Find("HUD");
+            _player = GameObject.Find("Player");
         }
 
-        // Update is called once per frame
         private void Update()
         {
         
@@ -22,6 +34,17 @@ namespace Cutscenes
         public void StartEndingScene()
         {
             Debug.Log("GameEndingScene::Start");
+            _HUD.SetActive(false);
+            StartCoroutine(EndingScene());
+        }
+
+        public IEnumerator EndingScene()
+        {
+            StartCoroutine(_blackScreen.FadeIn(4));
+            yield return new WaitForSeconds(4f);
+            _player.transform.position = _playerTransform.position;
+            yield return new WaitForSeconds(2f);
+            StartCoroutine(_blackScreen.FadeOut(4));
         }
     }
 }

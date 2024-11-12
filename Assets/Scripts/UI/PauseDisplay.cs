7073f17f21f5679e00e14ecb4314e8b55076ed9e
Piotr Rutkowski s27051 <s27051@pjwstk.edu.pl>
2024-11-12 11:28:32 +0100

    Added Exit to main menu and keybinds to pause menu

--- Assets/Scripts/UI/PauseDisplay.cs
+++ Assets/Scripts/UI/PauseDisplay.cs
@@ -1,6 +1,9 @@
 using System.Collections;
+using Level_Control;
+using Player;
 using UI.MenuController;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace UI
@@ -10,6 +13,9 @@ namespace UI
         private CanvasGroup _canvasGroup;
         private Image _selectionPanel1;
         private Image _selectionPanel2;
+        private Image _selectionPanel3;
+        private LevelLoader _levelLoader;
+
 
         private bool _displayActive;
         private bool _canExitPause;  
@@ -17,19 +23,28 @@ namespace UI
 
         private AudioManeger _audioManeger;
         private PauseMenuController _pauseMenuController;
+        private GameObject _player;
+        private PlayerController _playerController;
+
 
         private void Awake()
         {
             _canvasGroup = GetComponent<CanvasGroup>();
             _selectionPanel1 = GameObject.Find("SelectionPanel1").GetComponent<Image>();
             _selectionPanel2 = GameObject.Find("SelectionPanel2").GetComponent<Image>();
+            _selectionPanel3 = GameObject.Find("SelectionPanel3").GetComponent<Image>();
 
             _audioManeger = GameObject.FindWithTag("AudioManager")?.GetComponent<AudioManeger>();
             _pauseMenuController = FindObjectOfType<PauseMenuController>();
 
             SetVisible(_selectionPanel1, true);
             SetVisible(_selectionPanel2, false);
+            SetVisible(_selectionPanel3, false);
             _currentSelection = 0;
+            _player = GameObject.Find("Player");
+            _playerController = _player.GetComponent<PlayerController>();
+            _levelLoader = FindObjectOfType<LevelLoader>();
+
         }
 
         private void Update()
@@ -53,7 +68,7 @@ namespace UI
                 }
                 else if (yAxisInput < 0)
                 {
-                    _currentSelection = Mathf.Min(1, _currentSelection + 1); 
+                    _currentSelection = Mathf.Min(2, _currentSelection + 1); 
                 }
 
                 _audioManeger.PlayMenuSFX(_audioManeger.menuClick);
@@ -72,14 +87,22 @@ namespace UI
                 else if (_currentSelection == 1)
                 {
                     _pauseMenuController.ChangeMenu(_pauseMenuController.PlayerControls);
+                }else if (_currentSelection == 2)
+                {
+                    Time.timeScale = 1f;
+                    _playerController.FullGameReset();
+                    InputManager.PlayerDisable();
+                    InputManager.MenuEnable();
+                    _levelLoader.LoadNextLevel(0);
                 }
-            }
+            }   
         }
 
         private void UpdateSelectionPanels()
         {
             SetVisible(_selectionPanel1, _currentSelection == 0);
             SetVisible(_selectionPanel2, _currentSelection == 1);
+            SetVisible(_selectionPanel3, _currentSelection == 2);
         }
 
         private void SetVisible(Image image, bool visible)
